b4ef7aa8a23212451be78b0458ee7ac214d4bf07
Agustin Soria <soriaagustin8@gmail.com>
2023-12-06 09:24:59 -0300

    Creating reservation model

---
+++ app/models/reservation.rb
@@ -0,0 +1,8 @@
+class Reservation < ApplicationRecord
+    belongs_to :user
+    belongs_to :doctor
+  
+    def image_url
+      Rails.application.routes.url_helpers.url_for(image) if image.attached?
+    end
+  end
