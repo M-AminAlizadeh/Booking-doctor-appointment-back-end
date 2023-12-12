b219b4ded14d2bf9a4f845c7630efb8defc09e81
Agustin Soria <soriaagustin8@gmail.com>
2023-12-12 05:57:14 -0300

    Another try on root something

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
+  root to: redirect('/api-docs/index.html')
 
   namespace :v1 do
     post 'users/login' => 'users#login'
     post 'users/signup' => 'users#signup'
-    root 'users/signup' => 'users#signup'
     get 'users/fetch_current_user' => 'users#fetch_current_user'
     resources :doctors, only: [:create, :destroy, :show, :index]
     resources :reservations, only: [:create, :show, :index]
