9410c795373bb0be64cfff58cd5980f1532ae118
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-12-14 17:00:35 +0330

    Update cors origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,9 @@
-require 'rack/cors'
-
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins "http://localhost:3000"
-
-    resource "*",
-      headers: ["Access-Control-Allow-Origin", "Access-Control-Allow-Methods", "Access-Control-Allow-Headers"],
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    allow do
+      origins 'http://127.0.0.1:3001', 'http://localhost:3001', 'http://127.0.0.1:3000', 'http://localhost:3000'
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        credentials: true
+    end
   end
-end
