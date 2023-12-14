525b6769e20c9953aa2f3a9d12a8eec8e19089cc
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-12-14 15:33:40 +0330

    Add localhost domain to cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,16 +1,11 @@
-# Be sure to restart your server when you modify this file.
-
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin Ajax requests.
-
-# Read more: https://github.com/cyu/rack-cors
+require 'rack/cors'
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    origins "http://localhost:3000"
 
     resource "*",
-      headers: :any,
+      headers: ["Access-Control-Allow-Origin", "Access-Control-Allow-Methods", "Access-Control-Allow-Headers"],
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
