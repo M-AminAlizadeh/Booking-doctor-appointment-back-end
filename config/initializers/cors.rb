3480a57bddcc05f3140878967cf78080ff7ccad2
Agustin Soria <soriaagustin8@gmail.com>
2023-12-19 17:39:35 -0300

    Update to fetch from outside(front-end deployment)

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://booking-doctor-iqa1.onrender.com'
+    origins 'https://booking-doctor-iqa1.onrender.com', 'https://booking-doctor-appointment-front-end-git-76855a-m-aminalizadeh.vercel.app/'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
