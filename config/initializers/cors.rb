5f761c7005976b6198f7aa1f169ef44e297aecec
Agustin Soria <soriaagustin8@gmail.com>
2023-12-19 17:35:59 -0300

    Adding correct domain for the cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://booking-doctor-iqa1.onrender.com'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
