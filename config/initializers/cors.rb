4f322ad95676fde07687723eb3b16563aaa64444
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-12-20 13:20:17 +0330

    Change resource path

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'https://booking-doctor-appointment-front-end.vercel.app', 'http://localhost:3000'
-    resource '/api/*',
+    resource '/v1/*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
