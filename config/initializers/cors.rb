b22b6289a009f153a5760a636ca1e524d7c2f178
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-12-21 12:08:35 +0330

    Add localhost to origin in cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://booking-doctor-appointment-front-end.vercel.app', 'http://localhost:3000'
-    resource '/v1/*',
+    origins "http://localhost:3001", "http://localhost:3000" 
+
+    resource "*",
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
